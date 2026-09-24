e8ec8efd07cc23d89fe52897e9830f5b8df4b84f
Victor <victorlecouteur@gmail.com>
2026-09-24 03:34:37 +0200

    add greatest and least

--- sqlforge/inference/expression.py
+++ sqlforge/inference/expression.py
@@ -66,6 +66,9 @@ class ExprInference:
             case exp.Coalesce(this=head, expressions=tail):
                 return self._infer_coalesce([head, *tail])
 
+            case exp.Greatest(this=head, expressions=tail) | exp.Least(this=head, expressions=tail):
+                return self._infer_greatest_least([head, *tail])
+
             case _ if is_row(expression):
                 # TODO: postgresql doc 9.2
                 return NullSet.MAYBE_NULL
@@ -91,11 +94,14 @@ class ExprInference:
                     result = NullSet.MAYBE_NULL
         return result
 
-    def _infer_greatest(self) -> NullSet:
-        return NullSet.MAYBE_NULL
-
-    def _infer_least(self) -> NullSet:
-        return NullSet.MAYBE_NULL
+    def _infer_greatest_least(self, expressions: list[exp.Expr]) -> NullSet:
+        nulls = [self.infer_nullability(e) for e in expressions]
+        if any(n is NullSet.NON_NULL for n in nulls):
+            return NullSet.NON_NULL
+        elif all(n is NullSet.NULL for n in nulls):
+            return NullSet.NULL
+        else:
+            return NullSet.MAYBE_NULL
 
     # Boolean expressions
 
